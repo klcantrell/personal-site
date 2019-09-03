25332aad6055b4ad62144640bcf3fdcde765025c
klcantrell <cantrellkalalau@gmail.com>
2019-09-02 21:41:25 -0400

    convert styles and components from "hello-friend-starter" to typescript

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,46 +1,94 @@
 import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import Helmet from 'react-helmet';
+
+import Menu, { MenuItem } from './menu';
+
+import style from '../styles/header.module.css';
+
+interface SiteLogo {
+  src: string;
+  alt: string;
+}
 
 type Props = {
-  siteTitle: string;
+  siteLogo: SiteLogo;
+  logoText: string;
+  mainMenu: Array<MenuItem>;
+  mainMenuItems: number;
+  menuMoreText: string;
+  defaultTheme: string;
 };
 
-const Header = ({ siteTitle }: Props) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
+const Header = (props: Props) => {
+  const {
+    siteLogo,
+    logoText,
+    mainMenu,
+    mainMenuItems,
+    menuMoreText,
+    defaultTheme,
+  } = props;
+  const defaultThemeState =
+    (typeof window !== 'undefined' && window.localStorage.getItem('theme')) ||
+    null;
+  const [userTheme, changeTheme] = useState(defaultThemeState);
+  const [isMobileMenuVisible, toggleMobileMenu] = useState(false);
+  const [isSubMenuVisible, toggleSubMenu] = useState(false);
+  const onChangeTheme = () => {
+    const opositeTheme =
+      (userTheme || defaultTheme) === 'light' ? 'dark' : 'light';
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
+    changeTheme(opositeTheme);
 
-Header.defaultProps = {
-  siteTitle: ``,
+    typeof window !== 'undefined' &&
+      window.localStorage.setItem('theme', opositeTheme);
+  };
+  const onToggleMobileMenu = () => toggleMobileMenu(!isMobileMenuVisible);
+  const onToggleSubMenu = () => toggleSubMenu(!isSubMenuVisible);
+
+  return (
+    <>
+      <Helmet>
+        <body
+          className={
+            (userTheme || defaultTheme) === 'light'
+              ? 'light-theme'
+              : 'dark-theme'
+          }
+        />
+      </Helmet>
+      <header className={style.header}>
+        <div className={style.inner}>
+          <Link to="/">
+            <div className={style.logo}>
+              {siteLogo.src ? (
+                <img src={siteLogo.src} alt={siteLogo.alt} />
+              ) : (
+                <>
+                  <span className={style.mark}>&gt;</span>
+                  <span className={style.text}>{logoText}</span>
+                  <span className={style.cursor} />
+                </>
+              )}
+            </div>
+          </Link>
+          <span className={style.right}>
+            <Menu
+              mainMenu={mainMenu}
+              mainMenuItems={mainMenuItems}
+              isMobileMenuVisible={isMobileMenuVisible}
+              isSubMenuVisible={isSubMenuVisible}
+              menuMoreText={menuMoreText}
+              onToggleMobileMenu={onToggleMobileMenu}
+              onToggleSubMenu={onToggleSubMenu}
+              onChangeTheme={onChangeTheme}
+            />
+          </span>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React, { ReactNode } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
-import './layout.css';
+
+import '../styles/layout.css';
 
 type Props = {
   children: ReactNode;
@@ -21,28 +15,44 @@ const Layout = ({ children }: Props) => {
       site {
         siteMetadata {
           title
+          logo {
+            src
+            alt
+          }
+          logoText
+          defaultTheme
+          copyrights
+          mainMenu {
+            title
+            path
+          }
+          showMenuItems
+          menuMoreText
         }
       }
     }
   `);
+  const {
+    logo,
+    logoText,
+    defaultTheme,
+    mainMenu,
+    showMenuItems,
+    menuMoreText,
+  } = data.site.siteMetadata;
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div className="container">
+        <Header
+          siteLogo={logo}
+          logoText={logoText}
+          defaultTheme={defaultTheme}
+          mainMenu={mainMenu}
+          mainMenuItems={showMenuItems}
+          menuMoreText={menuMoreText}
+        />
+        <div className="content">{children}</div>
       </div>
     </>
   );
