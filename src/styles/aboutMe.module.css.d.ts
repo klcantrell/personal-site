d11e2d44122ead1093878d07bcc6bf9fdcc7999f
Kalalau Cantrell <cantrellkalalau@gmail.com>
2025-07-05 15:49:34 -0400

    Remove old personal site. Going to start fresh with Astro.

--- src/styles/aboutMe.module.css.d.ts
+++
@@ -1,3 +0,0 @@
-export const header: string;
-export const content: string;
-export const blockQuote: string;
--- src/styles/skills.module.css.d.ts
+++
@@ -1,4 +0,0 @@
-export const skill: string;
-export const skills: string;
-export const reasonLogo: string;
-export const reasonLogoContainer: string;
